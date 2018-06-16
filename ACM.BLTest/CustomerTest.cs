03ade35143ae88f5639a098405acf6d1bacedfe6
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-06-16 17:35:17 +0200

    Add Unittest CustomerClass

---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+
+            string expected = "Baggins, Bilbo";
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void FullFirsNameEmpty()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+           
+            customer.LastName = "Baggins";
+
+            string expected = "Baggins";
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void FullLastNameEmpty()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            
+
+            string expected = "Bilbo";
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
