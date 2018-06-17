3f45455e2286e0307f290072eacdab1a488727f9
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-06-17 15:23:38 +0200

    prepare adress repository for unit test

---
+++ ACM.BL/AddressRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class AddressRepository
+    {
+        //Retrieve one address.
+
+        public Address Retrieve(int addressId)
+        {
+            //create instance of the address class
+            //pass in the addressId
+            Address address = new Address(addressId);
+
+            //code that retrieves the defined address.
+
+            //Temporary hard coded values to return a polulated address
+            if (addressId == 1)
+            {
+                address.StreetLine1 = "Bag End";
+                address.StreetLine2 = "Bagshot Row";
+                address.City = "Hobbiton";
+                address.State = "Shire";
+                address.Country = "Middle Earth";
+                address.PostalCode = "144";
+
+            }
+            return address;
+        }
+        public IEnumerable<Address> RetrieveByCustomerId(int customerId)
+        {
+            //code that retrieves the defined addresses for the customer.
+            //Temporary hard coded values to return a set of  addresses for a customer
+            var addressList = new List<Address>();
+            Address address = new Address(1)
+            {
+                AddressType = 1,
+                StreetLine1 = "Bag End",
+                StreetLine2 = "Bagshot Row",
+                City = "Hobbiton",
+                State = "Shire",
+                Country = "Middle Earth",
+                PostalCode = "144",
+
+            };
+            addressList.Add(address);
+
+            address = new Address(2)
+            {
+                AddressType = 2,
+                StreetLine1 = "Green Dragon",
+                City = "Bywater",
+                State = "Shire",
+                Country = "Middle Earth",
+                PostalCode = "146",
+
+            };
+            addressList.Add(address);
+            return addressList;
+
+        }
+        //Saves the current address.
+        public bool Save(Address address)
+        {
+            //code that saves the defined address.
+            return true;
+
+        }
+    }
+}
--- ACM.BL/CustomerRepository.cs
+++ ACM.BL/CustomerRepository.cs
@@ -8,11 +8,17 @@ namespace ACM.BL
 {
     public class CustomerRepository
     {
+        private AddressRepository addressRepository { get; set; }
+        public CustomerRepository()
+        {
+            addressRepository = new AddressRepository();
+        }
         //retrieve one customer
         public Customer Retrieve(int customerId)
         {
             //create the instance of the customer
             Customer customer = new Customer(customerId);
+            customer.AddressList = addressRepository.RetrieveByCustomerId(customerId).ToList();
             //code that retrieves the difined customer
             //return new Customer();
             //Temporary hard coded values to return a populated customer
