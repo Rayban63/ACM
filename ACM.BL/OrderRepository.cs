dd81cdd004c06f3eb67fc35dcc28c41146aeeb5d
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-06-18 23:12:17 +0200

    prepare automated test for orderdisplay and orderdisplayitems

--- ACM.BL/OrderRepository.cs
+++ ACM.BL/OrderRepository.cs
@@ -24,6 +24,53 @@ namespace ACM.BL
             }
             return order;
         }
+        public OrderDisplay RetrieveOrderDisplay(int orderId)
+        {
+            OrderDisplay orderDisplay = new OrderDisplay();
+            //code that retrieves the defined order fields
+
+            //temporary hard-coded data
+
+            if(orderId == 10)
+            {
+                orderDisplay.FirstName = "Bilbo";
+                orderDisplay.Lastname = "Baggins";
+                orderDisplay.OrderDate = new DateTimeOffset(2018, 6, 18, 12, 00, 00, new TimeSpan(07, 0, 0));
+                orderDisplay.ShippingAddress = new Address()
+                {
+                    AddressType = 1,
+                    StreetLine1 = "Bag End",
+                    StreetLine2 = "Bagshot Row",
+                    City = "Hobbiton",
+                    State = "Shire",
+                    Country = "Middle Earth",
+                    PostalCode = "144"
+                };
+            }
+            orderDisplay.OderDisplayItemList = new List<OrderDisplayItem>();
+            //code that retrieves the ordered items
+
+            //temporary hard-coded data
+            if(orderId == 10)
+            {
+                var orderDisplayItem = new OrderDisplayItem()
+                {
+                    ProductName = "SunFlowers",
+                    PurchasePrice = 15.99M,
+                    OderQuantity = 2
+                };
+                orderDisplay.OderDisplayItemList.Add(orderDisplayItem);
+                orderDisplayItem = new OrderDisplayItem()
+                {
+                    ProductName = "Rake",
+                    PurchasePrice = 56.33M,
+                    OderQuantity = 2
+                };
+                orderDisplay.OderDisplayItemList.Add(orderDisplayItem);
+            }
+            return orderDisplay;
+
+        }
 
         //Saves the current order
         public bool Save()
